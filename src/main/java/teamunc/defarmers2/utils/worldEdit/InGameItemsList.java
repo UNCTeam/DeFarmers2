292f3d96d2a357e81acc0b433de2f1678bb19d61
Delsus78 <delsustheugm@gmail.com>
2022-09-08 00:30:43 +0200

    adding random 5 choosen items

--- src/main/java/teamunc/defarmers2/utils/worldEdit/InGameItemsList.java
+++ src/main/java/teamunc/defarmers2/utils/worldEdit/InGameItemsList.java
@@ -1,24 +1,36 @@
 package teamunc.defarmers2.utils.worldEdit;
 
 import java.io.Serializable;
-import java.util.HashMap;
-import java.util.Random;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class InGameItemsList implements Serializable {
 
-    private HashMap<String,DoubleValue> itemsListWithPrice;
-    private long seed;
+    private final HashMap<String,DoubleValue> itemsListWithPrice;
+    private long seed = 0;
 
     //getter
     public HashMap<String, Integer> getItemsListWithPrice() {
-        this.seed = new Random().nextInt(1000000);
+        if (this.seed == 0) this.seed = new Random().nextInt(1000000);
         HashMap<String,Integer> itemsListWithPriceEnd = new HashMap<>();
 
         for (String key : itemsListWithPrice.keySet()) {
             itemsListWithPriceEnd.put(key,getRandomValueFromSeed(itemsListWithPrice.get(key)));
         }
 
-        return itemsListWithPriceEnd;
+        // keep only 5 items in the list
+        HashMap<String,Integer> res = new HashMap<>();
+        int i = 0;
+        ArrayList<String> keys = new ArrayList<>(itemsListWithPriceEnd.keySet());
+        Collections.shuffle(keys,new Random(seed));
+        for (String key : keys) {
+            if (i < 5) {
+                res.put(key, itemsListWithPriceEnd.get(key));
+                i++;
+            }
+        }
+
+        return res;
     }
 
     private Integer getRandomValueFromSeed(DoubleValue minMaxValue) {
