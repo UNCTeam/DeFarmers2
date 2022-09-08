8f200cc6f4602e80d14e14d1b392f3f9c7794d72
Delsus78 <delsustheugm@gmail.com>
2022-09-08 08:23:23 +0200

    changing ArmoredItem system

--- src/main/java/teamunc/defarmers2/customsItems/ArmoredItem.java
+++ src/main/java/teamunc/defarmers2/customsItems/ArmoredItem.java
@@ -1,7 +1,10 @@
 package teamunc.defarmers2.customsItems;
 
+import com.sk89q.worldedit.util.Direction;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Mob;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
@@ -9,7 +12,12 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.scoreboard.Team;
 import org.jetbrains.annotations.NotNull;
 import teamunc.defarmers2.managers.TeamManager;
+import teamunc.defarmers2.mobs.EnumMobStatue;
+import teamunc.defarmers2.serializables.MobEffect;
+import teamunc.defarmers2.utils.worldEdit.MathsUtils;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
 
@@ -25,11 +33,20 @@ public class ArmoredItem extends CustomItem {
         TeamManager teamManager = TeamManager.getInstance();
 
         Team team = teamManager.getTeamOfPlayer(player.getName());
-
-        for (UUID uuid : teamManager.getMobsSpawnedOfTeam(team.getName())) {
-            Mob mob = (Mob) Bukkit.getEntity(uuid);
-            mob.getEquipment().setHelmet(new ItemStack(Material.DIAMOND_HELMET));
-            mob.getEquipment().setChestplate(new ItemStack(Material.DIAMOND_CHESTPLATE));
+        Location location = MathsUtils.getNextLocation(Direction.DOWN, player.getLocation(), 64, null);
+        if (location != null) {
+            // get mobs in a rayon of 15 blocks around the location
+            Collection<Entity> entities = location.getWorld().getNearbyEntities(location, 10, 10, 10);
+            int i = 0;
+            for (Entity entity : entities) {
+                if (i >= 10) break;
+                if (entity instanceof Mob && teamManager.getMobsSpawnedOfTeam(team.getName()).contains(entity.getUniqueId())) {
+                    Mob mob = (Mob) entity;
+                    mob.getEquipment().setHelmet(new ItemStack(Material.IRON_HELMET));
+                    mob.getEquipment().setChestplate(new ItemStack(Material.IRON_CHESTPLATE));
+                    i++;
+                }
+            }
         }
     }
     @Override
